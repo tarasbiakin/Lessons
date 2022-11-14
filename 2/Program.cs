b214b8d2d0c25fac99b8eda7cf6b1753def0175c
tarasbiakin <115660593+tarasbiakin@users.noreply.github.com>
2022-11-14 19:17:18 +0300

    2

--- 2/Program.cs
+++ 2/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Составить программу вывода на экран «столбиком» пяти любых чисел, введенных пользователем с клавиатуры.
+
+System.Console.WriteLine("Введите числа");
+int[]array = new int[5];
+for(int i = 0; i <array.Length; i++ ){
+    array[i] = Convert.ToInt16(Console.ReadLine());
+}
+foreach(int el in array){
+    System.Console.WriteLine("Число " +el);
+}
