fdc0b14158343a3fb28fa33d0d5c0260f9b25a9f
tarasbiakin <115660593+tarasbiakin@users.noreply.github.com>
2022-11-14 19:00:35 +0300

    1

---
+++ 1/Program.cs
@@ -0,0 +1,14 @@
+﻿//Составить программу вывода на экран в одну строку четырех любых чисел с тремя пробелами между ними.
+
+using System;
+namespace Lessons{
+    class Program{
+        static void Main(){
+            for(int i=1;i<5;i++){
+            Random a = new Random();
+            int randomNober = a.Next(1,50);
+            System.Console.WriteLine(randomNober);}
+
+        }
+    }
+}
