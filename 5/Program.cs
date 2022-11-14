6d421fb2598889497a73e744ca38b06b8b8dafe4
tarasbiakin <115660593+tarasbiakin@users.noreply.github.com>
2022-11-14 19:49:08 +0300

    5

---
+++ 5/Program.cs
@@ -0,0 +1,9 @@
+﻿//Вывести на экран число e (основание натурального логарифма) с точностью до десятых.
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double e = Math.E;
+        System.Console.WriteLine(Math.Round(e,1));
+    }
+}
