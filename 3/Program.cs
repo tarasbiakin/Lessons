e86e4e70d629b707d4fa41744bdc80f51423e1a0
tarasbiakin <115660593+tarasbiakin@users.noreply.github.com>
2022-11-14 19:30:20 +0300

    3

---
+++ 3/Program.cs
@@ -0,0 +1,10 @@
+﻿// Вывести на экран число «ПИ» с точностью до сотых.
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double pi = Math.PI;
+        System.Console.WriteLine(Math.Round(pi,2));
+    }
+}
