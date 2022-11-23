18843d0607621cd6b22782357236364bffd3a581
tarasbiakin <115660593+tarasbiakin@users.noreply.github.com>
2022-11-23 11:59:10 +0300

    12 decimal

---
+++ 12/Program.cs
@@ -0,0 +1,22 @@
+﻿// За каждый месяц банк начисляет к сумме вклада 7% от суммы. Напишите консольную программу, в которую пользователь вводит сумму вклада и количество месяцев. А банк вычисляет конечную сумму вклада с учетом начисления процентов за каждый месяц.
+// Для вычисления суммы с учетом процентов используйте цикл for. Для ввода суммы вклада используйте выражение Convert.ToDecimal(Console.ReadLine()) (сумма вклада будет представлять тип decimal).
+
+Console.WriteLine($"Введите размер вклада");
+decimal vkl =Convert.ToDecimal(Console.ReadLine());
+
+Console.WriteLine($"Введите размер процента");
+decimal prc = Convert.ToDecimal(Console.ReadLine());
+
+Console.WriteLine($"Введите срок");
+decimal sr = Convert.ToDecimal(Console.ReadLine());
+
+
+
+void Vkl(decimal vk,decimal pr,decimal s){
+    decimal res=vk;
+    for(int i=0;i<=s;i++){
+        res =res*pr; 
+    }Console.WriteLine("Результат: "+res);
+    
+}
+Vkl( vkl,prc,sr);
